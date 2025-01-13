43286f12d5456067480c0e97657b81a150ec6d8d
edeeeen <okaythisisepic69420@gmail.com>
2025-01-12 19:01:08 -0500

    initial commit

--- FFLogsSearch/Plugin.cs
+++ FFLogsSearch/Plugin.cs
@@ -1,9 +1,17 @@
-﻿using Dalamud.Game.Command;
+﻿using System;
+using System.Diagnostics;
+using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using System.IO;
+using System.Runtime.InteropServices;
+using Dalamud.Game.ClientState.Conditions;
+using Dalamud.Game.Gui.ContextMenu;
+using Dalamud.Game.Text;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using Lumina.Excel.Sheets;
 using SamplePlugin.Windows;
 
 namespace SamplePlugin;
@@ -17,15 +25,17 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IPluginLog Log { get; private set; } = null!;
 
-    private const string CommandName = "/pmycommand";
+    private const string CommandName = "/fflogs";
 
     public Configuration Configuration { get; init; }
 
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
+    public readonly WindowSystem WindowSystem = new("FFLogsSearch");
     private ConfigWindow ConfigWindow { get; init; }
     private MainWindow MainWindow { get; init; }
-
-    public Plugin()
+    
+    private IDataManager DM { get; init; }
+    public IContextMenu ContextMenu { get; init; }
+    public Plugin(IDataManager dataManager, IContextMenu contextMenu)
     {
         Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
 
@@ -54,8 +64,12 @@ public sealed class Plugin : IDalamudPlugin
 
         // Add a simple message to the log with level set to information
         // Use /xllog to open the log window in-game
-        // Example Output: 00:57:54.959 | INF | [SamplePlugin] ===A cool log message from Sample Plugin===
+        // Example Output: 00:57:54.959 | INF | [FFLogsSearch] ===A cool log message from Sample Plugin===
         Log.Information($"===A cool log message from {PluginInterface.Manifest.Name}===");
+        
+        this.ContextMenu = contextMenu;
+        this.ContextMenu.OnMenuOpened += this.OnContextMenuOpened;
+        this.DM = dataManager;
     }
 
     public void Dispose()
@@ -64,16 +78,64 @@ public sealed class Plugin : IDalamudPlugin
 
         ConfigWindow.Dispose();
         MainWindow.Dispose();
-
+        
+        this.ContextMenu.OnMenuOpened -= this.OnContextMenuOpened;
+        
         CommandManager.RemoveHandler(CommandName);
     }
 
     private void OnCommand(string command, string args)
     {
+        OpenUrl("https://www.fflogs.com/search/?term=" + args);
         // in response to the slash command, just toggle the display status of our main ui
-        ToggleMainUI();
     }
 
+    private void OnContextMenuOpened(IMenuOpenedArgs args)
+    {
+
+
+        if (args.Target is MenuTargetDefault defMen && defMen.TargetName != null)
+        {
+            args.AddMenuItem(new()
+            {
+                OnClicked = (_) =>
+                {
+                    OpenUrl("https://www.fflogs.com/search/?term=" + defMen.TargetName);
+                },
+                Prefix = SeIconChar.BoxedLetterF,
+                Name = "Search FFLogs",
+                PrefixColor = 15
+            });
+        }
+    }
+    private void OpenUrl(string url)
+    {
+        try
+        {
+            Process.Start(url);
+        }
+        catch
+        {
+            // hack because of this: https://github.com/dotnet/corefx/issues/10361
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+            else
+            {
+                throw;
+            }
+        }
+    }
     private void DrawUI() => WindowSystem.Draw();
 
     public void ToggleConfigUI() => ConfigWindow.Toggle();
